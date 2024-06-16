b2599e253bc3fdeacee2696dff81084189ec60d3
jjangg96 <jjangg96@gmail.com>
2024-06-17 02:50:24 +0900

    add swap price in berachain-v2

--- src/availables/testnet/berachain-v2.ts
+++ src/availables/testnet/berachain-v2.ts
@@ -163,7 +163,7 @@ export default class BerachainV2 extends TendermintBerachain {
                 "Referer": "https://bartio.bex.berachain.com/",
             }
         }).then(response => {
-            this.availableGauge.labels('bera', 'honey').set(response.data.data.tokenHoneyPrice.price);
+            this.availableGauge.labels('bera', 'honey').set(parseFloat(response.data.data.tokenHoneyPrice.price));
         });
     }
 
