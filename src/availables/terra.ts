cff43632c9cc3c189db5cc9005b7ceaf68c4b622
JinWoo Lee <jjangg96@gmail.com>
2026-04-13 18:28:10 +0900

    refactor: consolidate cosmos collector profiles

--- src/availables/terra.ts
+++ src/availables/terra.ts
@@ -1,49 +1,36 @@
 import Tendermint from "./tendermint-v1beta1";
-import {Gauge} from "prom-client";
+import { Gauge } from "prom-client";
 // import * as _ from "lodash";
 
 export default class Terra extends Tendermint {
-    private readonly missedOracleGauge = new Gauge({
-        name: `${this.metricPrefix}_missed_oracle_count`,
-        help: 'Missed oracle count',
+  private readonly missedOracleGauge = new Gauge({
+    name: `${this.metricPrefix}_missed_oracle_count`,
+    help: "Missed oracle count",
+  });
+
+  public constructor(
+    protected readonly existMetrics: string,
+    protected readonly apiUrl: string,
+    protected readonly rpcUrl: string,
+    protected readonly addresses: string,
+    protected readonly validator: string,
+  ) {
+    super(existMetrics, apiUrl, rpcUrl, addresses, validator);
+
+    this.registry.registerMetric(this.missedOracleGauge);
+  }
+
+  public async makeMetrics(): Promise<string> {
+    await this.updateMissedOracle(this.validator);
+    return await super.makeMetrics();
+  }
+
+  private async updateMissedOracle(validator: string): Promise<void> {
+    const url = `${this.apiUrl}/terra/oracle/v1beta1/validators/${validator}/miss`;
+
+    return this.get(url, (response) => {
+      const missedOracle = Number(response.data.miss_counter);
+      this.missedOracleGauge.set(Number.isFinite(missedOracle) ? missedOracle : 0);
     });
-
-    public constructor(protected readonly existMetrics: string,
-                       protected readonly apiUrl: string,
-                       protected readonly rpcUrl: string,
-                       protected readonly addresses: string,
-                       protected readonly validator: string) {
-        super(existMetrics, apiUrl, rpcUrl, addresses, validator);
-
-        this.registry.registerMetric(this.missedOracleGauge);
-    }
-
-    public async makeMetrics(): Promise<string> {
-        await this.updateMissedOracle(this.validator);
-        return await super.makeMetrics();
-    }
-
-    private async updateMissedOracle(validator: string): Promise<void> {
-        const url = `${this.apiUrl}/terra/oracle/v1beta1/validators/${validator}/miss`;
-
-        return this.get(url, response => {
-            this.missedOracleGauge.set(parseInt(response.data.miss_counter));
-        });
-    }
-
-    // protected async updateRank(validator: string): Promise<void> {
-    //     const url = `${this.apiUrl}/cosmos/staking/v1beta1/validators?status=BOND_STATUS_BONDED&pagination.limit=150`;
-    //
-    //     return this.get(url, response => {
-    //         const sorted = _.sortBy(response.data.validators, (o) => {
-    //             return parseInt(o.tokens);
-    //         }).reverse();
-    //
-    //         const rank = _.findIndex(sorted, (o) => {
-    //             return o.operator_address === validator;
-    //         }) + 1;
-    //
-    //         this.rankGauge.labels(validator).set(rank);
-    //     });
-    // }
+  }
 }
